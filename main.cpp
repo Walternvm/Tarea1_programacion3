2a2e4209ef4649cf8c9f98db164b154981ddab24
walteraquino <walter.aquino@utec.edu.pe>
2026-04-01 13:07:27 -0500

    Inicializar

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Tensor{
+    double x,y,z;
+    double*** arr;
+    public:
+        Tensor(const vector<size_t>& shape, const vector<double>& values){
+        }
+
+};
+
+int main(){
+    return 0;
+}
